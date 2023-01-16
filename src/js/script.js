f100e435d9a947b620cf3c868b4ffba96ce72d39
Rodjfreitas <rodrigofreitas2011@live.com>
2023-01-16 12:28:15 -0300

    adicionando códigos java script para transição e seleção de cards

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,47 @@
+const suvButton = document.querySelector('.suvs-btn');
+const luxButton = document.querySelector('.luxury-btn');
+const sedButton = document.querySelector('.sedans-btn');
+const backButton = document.querySelector('.back-btn');
+const cardSedan = document.querySelector('.one');
+const cardSuv = document.querySelector('.two');
+const cardLux = document.querySelector('.three');
+
+const Orange = document.querySelector('.infoA');
+const Green = document.querySelector('.infoB');
+const GreenIntense = document.querySelector('.infoC');
 
 
+console.log(sedButton);
+
+function sedanTransition (){  
+    Orange.style.display = 'block';
+    cardSuv.style.display = 'none';
+    cardLux.style.display = 'none';
+    sedButton.style.visibility = 'hidden';
+
+} 
+
+function suvTransition (){
+    Green.style.display = 'block';
+    cardSedan.style.display = 'none';
+    cardLux.style.display = 'none';
+    suvButton.style.visibility = 'hidden';
+}
+
+function luxTransition (){
+    GreenIntense.style.display = 'block';
+    cardSedan.style.display = 'none';
+    cardSuv.style.display = 'none';
+    luxButton.style.visibility = 'hidden';
+}
+
+function backTransition (){
+  location.reload();
+}
+
+
+
+sedButton.addEventListener('click',sedanTransition);
+suvButton.addEventListener('click',suvTransition);
+luxButton.addEventListener('click',luxTransition);
+backButton.addEventListener('click',backTransition);
